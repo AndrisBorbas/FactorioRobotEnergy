bc1bb3879e5ad8ab3461409b38ee01b6756142cd
Andris Borbás <andris.borbas@gmail.com>
2020-12-26 04:40:32 +0100

    Reorganize and Factorio 1.1 compatibility

--- epic_robot_energy_4.0.0/robot3.lua
+++
@@ -1,2 +0,0 @@
-data.raw["logistic-robot"]["logistic-robot"].max_energy = "6MJ"
-data.raw["construction-robot"]["construction-robot"].max_energy = "6MJ"
--- epic_robot_energy_5.0.0/robot3.lua
+++
@@ -1,3 +0,0 @@
-EV = settings.startup["epic_robot_energy-Multi"].value * 1.5
-data.raw["logistic-robot"]["logistic-robot"].max_energy = (tostring(EV) .. "MJ")
-data.raw["construction-robot"]["construction-robot"].max_energy = (tostring(EV) .. "MJ")
